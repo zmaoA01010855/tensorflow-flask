df62b0a974562fd19600dda5c70b529b75438872
ziming <brucedayton1970@yahoo.com>
2020-06-07 01:07:49 +0000

    add tutorial CONNECTING MODEL TO THE SERVER

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 from flask_bootstrap import Bootstrap
-
+import os
+import inference
 
 app = Flask(__name__)
 Bootstrap(app)
 
-
 """
 routes
 """
-
-@app.route('/',methods=['GET'])
+@app.route('/',methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            image_path = os.path.join('static', uploaded_file.filename)
+            uploaded_file.save(image_path)
+            class_name = inference.get_prediction(image_path)
+            print('CLASS NAME=', class_name)
     return render_template('index.html')
 
 if __name__ == "__main__":
